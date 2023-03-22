31c555e985d061e60cb4e21df5d2861220a20315
marciopsj <marciopsj@yahoo.com.br>
2023-03-21 21:12:37 -0300

    js excercice

--- Formação JavaScript Developer/aulas/07 - Importação/main.js
+++ Formação JavaScript Developer/aulas/07 - Importação/main.js
@@ -4,4 +4,14 @@ console.log(funcoes.gets()); */
 
 const { gets, print } = require('./funcoes-auxiliares');
 
-print(gets());
+
+let maiorValorEncontrado = 0;
+
+for (let i = 0; i < 5; i++) {
+    const numeroSorteado = gets();
+    if (numeroSorteado > maiorValorEncontrado) {
+        maiorValorEncontrado = numeroSorteado;
+    }
+}
+
+print(maiorValorEncontrado);
