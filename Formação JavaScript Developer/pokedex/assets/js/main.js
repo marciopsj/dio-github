e485f25666286b3bf6ecaec99aad27807bba9ae3
marciopsj <marciopsj@yahoo.com.br>
2023-03-27 22:27:10 -0300

    pokeapi - api study

--- Formação JavaScript Developer/pokedex/assets/js/main.js
+++ Formação JavaScript Developer/pokedex/assets/js/main.js
@@ -3,11 +3,39 @@ const print = (text) => {
     return console.log(text)
 }
 
-const offset = 0;
-const limit = 5;
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+const pokemonToItemList = (pokemon) => {
+    return `
+        <li class="content__item item--grass">
+            <div class="item__header">
+                <h3 class="item__name">${pokemon.name}</h3>
+                <span class="item__number">#001</span>
+            </div>
+            <div class="item__info">
+                <ul class="item__types">
+                    <li class="item__type">Grass</li>
+                    <li class="item__type">Poison</li>
+                </ul>
+                <img
+                    class="item__img"
+                    src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+                    alt="${pokemon.name}"
+                />
+            </div>
+        </li>
+    `
+}
 
-fetch(url)
-.then((response) => response.json())
-.then((jsonBody) => print(jsonBody))
-.catch((error) => print(error))
+const contentItems = document.querySelector('.content__items')
+
+pokeApi.getPokemons()
+    .then((pokemons) => {
+        const contentItem = []
+        for (let i = 0; i < pokemons.length; i++) {
+            const pokemon = pokemons[i]
+            contentItem.push(pokemonToItemList(pokemon))
+        }
+        print(contentItem) 
+    })
+    .catch((error) => print(error))
+
+     
