76482cc5d9d8e8565b26b44d14292f71fc1b2af6
marciopsj <marciopsj@yahoo.com.br>
2023-05-05 22:49:04 -0300

    fetching profile data

--- Formação JavaScript Developer/projectLab-Portfolio/assets/js/main.js
+++ Formação JavaScript Developer/projectLab-Portfolio/assets/js/main.js
@@ -1,5 +1,14 @@
+function updateProfileDara(profileData) {
+    const profilePhoto = document.querySelector('.profile__photo')
+    console.log(profilePhoto);
+    profilePhoto.src = profileData.photo
+    profilePhoto.alt = `Foto do perfil de ${profileData.name}`  
+
+
+}
+
 
 (async () => {
     const profileData = await fetchProfileData()
-    console.log(profileData)
+    updateProfileDara(profileData)
 })()
