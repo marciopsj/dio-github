7ae44bbf7053463a68030c1b3ded6acf23eb51df
marciopsj <marciopsj@yahoo.com.br>
2023-05-05 23:18:06 -0300

    populating profile data dynamically

--- Formação JavaScript Developer/projectLab-Portfolio/assets/js/main.js
+++ Formação JavaScript Developer/projectLab-Portfolio/assets/js/main.js
@@ -1,10 +1,25 @@
 function updateProfileDara(profileData) {
-    const profilePhoto = document.querySelector('.profile__photo')
-    console.log(profilePhoto);
+    const profilePhoto = document.querySelector('.profile__photo')        
+    
     profilePhoto.src = profileData.photo
-    profilePhoto.alt = `Foto do perfil de ${profileData.name}`  
-
-
+    profilePhoto.alt = `Foto do perfil de ${profileData.name}`    
+    
+    const profileName = document.querySelector('.profile__name')
+    profileName.innerText = profileData.name
+    
+    const profileJob = document.querySelector('.profile__job')
+    profileJob.innerText = profileData.job
+    
+    const profileLocation = document.querySelector('.profile__location')
+    profileLocation.innerText = profileData.location
+    
+    const profilePhone = document.querySelector('.profile__phone')
+    profilePhone.href = `tel:${profileData.phone}` 
+    profilePhone.innerText = profileData.phone
+    
+    const profileEmail = document.querySelector('.profile__email')
+    profileEmail.href = `mailto:${profileData.email}`
+    profileEmail.innerText = profileData.email 
 }
 
 
