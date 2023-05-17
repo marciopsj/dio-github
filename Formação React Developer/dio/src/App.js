bfe24201c1082ca7786a53687c4bd73d66408795
marciopsj <marciopsj@yahoo.com.br>
2023-05-16 21:08:22 -0300

    react component study

--- Formação React Developer/dio/src/App.js
+++ Formação React Developer/dio/src/App.js
@@ -1,7 +1,13 @@
+import Button from './components/Button'
+
 function App() {
   return (
     <div className="App">
       <p>Olá, React </p>
+      <Button title="Abrir" />
+      <Button title="Salvar" />
+      <Button title="Fechar" />
+
     </div>
   );
 }
