a833ea034bc7a7509dd6acc3860a45995aaac04a
marciopsj <marciopsj@yahoo.com.br>
2023-06-02 22:36:56 -0300

    prettier config

--- 04 - Formação React Developer/newReactProject/src/App.jsx
+++ 04 - Formação React Developer/newReactProject/src/App.jsx
@@ -1,31 +1,31 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react'
 
 const App = () => {
-  const [usuario, setUsuario] = useState("");
-  const [usuarios, setUsuarios] = useState(["Marcio", "Enzo", "Liebe"]);
-  const [count, setCount] = useState(0);
+  const [usuario, setUsuario] = useState('')
+  const [usuarios, setUsuarios] = useState(['Marcio', 'Enzo', 'Liebe'])
+  const [count, setCount] = useState(0)
 
   const handleAddUser = () => {
     if (usuario === '') {
-        return alert('Informe um nome')
+      return alert('Informe um nome')
     }
-    setUsuarios([usuario, ...usuarios]);
-    setUsuario("");
-  };
+    setUsuarios([usuario, ...usuarios])
+    setUsuario('')
+  }
 
   const handleClear = () => {
-    setUsuarios(["Marcio", "Enzo", "Liebe"]);
-    setUsuario("");
-  };
+    setUsuarios(['Marcio', 'Enzo', 'Liebe'])
+    setUsuario('')
+  }
 
   useEffect(() => {
-    setCount(usuarios.length);
-  }, [usuarios]);
+    setCount(usuarios.length)
+  }, [usuarios])
 
   return (
     <div className="App">
       <h1>Hello React</h1>
-      <h3>Total: {count}</h3>    
+      <h3>Total: {count}</h3>
       <form onSubmit={(e) => e.preventDefault()}>
         <input
           type="text"
@@ -52,7 +52,7 @@ const App = () => {
         </tbody>
       </table>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
