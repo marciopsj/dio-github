efcc9f290d8226f21fb392c7f04a2bc549633c64
marciopsj <marciopsj@yahoo.com.br>
2023-06-11 23:19:33 -0300

    React Router study

---
+++ 04 - Formação React Developer/test-react-router/src/App.js
@@ -0,0 +1,21 @@
+import {Home} from './pages/home'
+import {Login} from './pages/login'
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} /> 
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
