e885e997c13f5493a9ac336aab7b87becdfc7000
marciopsj <marciopsj@yahoo.com.br>
2023-06-14 23:47:06 -0300

    Button component

---
+++ 04 - Formação React Developer/dio-clone/src/components/Button/styled.js
@@ -0,0 +1,30 @@
+import { styled, css } from 'styled-components'
+
+export const ButtonContainer = styled.button`
+  position: relative;
+  background-color: #565656;
+  padding: 2px 12px;
+  min-width: 120px;
+  width: 100%;
+  color: #ffffff;
+  border-radius: 22px;
+
+  ${({ variant }) =>
+    variant !== 'primary' &&
+    css`
+      background-color: #e4105d;
+      min-width: 167px;
+      height: 33px;
+
+      &::after {
+        content: '';
+        position: absolute;
+        border: 1px solid #e4105d;
+        top: -5px;
+        left: -6px;
+        width: calc(100% + 10px);
+        height: calc(100% + 10px);
+        border-radius: 22px;
+      }
+    `}
+`
---
+++ 04 - Formação React Developer/dio-clone/src/pages/home/index.jsx
@@ -0,0 +1,12 @@
+import { Button } from '../../components/Button'
+
+const Home = () => {
+  return (
+    <>
+      <Button title="Teste 1" />
+      <Button title="Teste 2" variant="secondary"></Button>
+    </>
+  )
+}
+
+export { Home }
