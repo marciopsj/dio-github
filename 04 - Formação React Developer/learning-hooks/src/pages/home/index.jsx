ebe1b105a13460da04ac19459cbbfc497e53fe2f
mpessanha <marciopsj@yahoo.com.br>
2023-07-31 22:16:59 -0300

    useContext

--- 04 - Formação React Developer/learning-hooks/src/pages/home/index.jsx
+++ 04 - Formação React Developer/learning-hooks/src/pages/home/index.jsx
@@ -1,9 +1,12 @@
 import { Link } from 'react-router-dom'
+import { useContext } from 'react'
+import { UserContext } from '../contexts/userContext'
 
 export const Home = () => {
+    const user = useContext(UserContext)
     return (
         <>
-            <h1>Vamos aprender sobre os Hooks do React!</h1>
+            <h1>{user.userName}, vamos aprender sobre os Hooks do React!</h1>
             <Link to="/useState">useState</Link>
             <br />
             <Link to="/useEffect">useEffect</Link>
--- 04 - Formação React Developer/learning-hooks/src/routes.jsx
+++ 04 - Formação React Developer/learning-hooks/src/routes.jsx
@@ -5,10 +5,12 @@ import { UseEffect } from './pages/useEffect'
 import { UseCallback } from './pages/useCallback'
 import { UseMemo } from './pages/useMemo'
 import { UseRef } from './pages/useRef'
+import UserContextProvider from './pages/contexts/userContext'
 
 export const AppRoutes = () => {
     return (        
         <BrowserRouter>
+        <UserContextProvider>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/useState" element={<UseState />} />
@@ -16,10 +18,8 @@ export const AppRoutes = () => {
                 <Route path="/useCallback" element={<UseCallback />} />
                 <Route path="/useMemo" element={<UseMemo />} />
                 <Route path="/useRef" element={<UseRef />} />
-
-
-
             </Routes>
+        </UserContextProvider>
         </BrowserRouter>
     )
 }
